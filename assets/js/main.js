0098a89c760a5a21be46288cf003b0275f8825a1
Harsh Gupta <Harshdeepgupta49@gmail.com>
2021-06-10 09:48:06 +0530

    add changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -279,3 +279,21 @@
   });
 
 })()
+
+
+// counter count js 
+
+$(document).ready(function() {
+  $('.counter').each(function () {
+  $(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+  }, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+  $(this).text(Math.ceil(now));
+  }
+  });
+  });
+
+  });
